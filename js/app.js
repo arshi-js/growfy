26aa18e1b781ad5412e8559486afbfac769ff357
arshidin <arshitechosmanov@gmail.com>
2023-02-19 23:19:58 +0600

    Init

---
+++ js/app.js
@@ -0,0 +1,32 @@
+(() => {
+    "use strict";
+    function isWebp() {
+        function testWebP(callback) {
+            let webP = new Image;
+            webP.onload = webP.onerror = function() {
+                callback(2 == webP.height);
+            };
+            webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+        }
+        testWebP((function(support) {
+            let className = true === support ? "webp" : "no-webp";
+            document.documentElement.classList.add(className);
+        }));
+    }
+    let addWindowScrollEvent = false;
+    setTimeout((() => {
+        if (addWindowScrollEvent) {
+            let windowScroll = new Event("windowScroll");
+            window.addEventListener("scroll", (function(e) {
+                document.dispatchEvent(windowScroll);
+            }));
+        }
+    }), 0);
+    document.addEventListener("click", documentClick);
+    function documentClick(e) {
+        const targetItem = e.target;
+        if (targetItem.closest(".icon-menu")) document.documentElement.classList.toggle("menu-open");
+    }
+    window["FLS"] = true;
+    isWebp();
+})();
